71ee8ea324ddbdc5094ce94d2682a3c7931b51ba
Joseph Tran <fred.smoth@gmail.com>
2017-04-08 18:29:39 +1000

    Added in the start to Assignment 1

---
+++ Assignment 1/q1.m
@@ -0,0 +1,3 @@
+% Dichotomiser - Neural Networks
+% 1.1 plot the error curve for 10 cycles
+% 1.2 get error curve, how do w7 and w301 classify the entire training set?
---
+++ Assignment 1/q2.m
@@ -0,0 +1,8 @@
+% Fuzzy logic
+% 2.1.1 Find R = M X A
+
+
+% 2.1.2 Find A1a = M1 o R
+% 2.1.3 Find A1b = M1 o R
+% 2.2.1 Use MoM to find output voltage
+% 2.2.2 Use CoA to find output voltage
