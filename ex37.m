fbb561e10e1c6c1d1d40fc3d0ad41275c47513c7
Joseph Tran <fred.smoth@gmail.com>
2017-05-14 20:57:42 +1000

    Semi functioning neural network, still has some bugs in it and doesnt quite get the values set in the example

--- ex37.m
+++ ex37.m
@@ -1,4 +1,13 @@
 % exercise 3.2
+% look at this for a helpful guide
+
+% steps
+% 1. run foward as normal, calculate inputs into first layer
+% 2. use first layer as "inputs" for second layer + bias value
+% 3. continue until you hit the end
+% 4. then go backwards to adjust weights using outputs as inputs to last
+% layer
+% 5. keep going until you reach the first layer
 
 clc;
 clear;
@@ -10,52 +19,65 @@ x4 = [1; 1];
 
 d = [-1, 1, -1, 1];
 
+bias = -1;
+
 y = [x1, x2, x3, x4];
-y(3, :) = -1;
+y(3, :) = bias;
 
+% w is the weights for the outputs
 w = [-0.8568, 0.3998, -1.0702];
-
+% wBar is the weights for the inputs
 wBar = [-6.9938, 6.6736, 1.555; -4.2812, 3.9127, 3.6233];
 
-runDeepLearning(y, d, 0.1, w, wBar, 5);
+% nuConstant
+n = 0.1;
 
-function output = runDeepLearning(augmentedInputs, expectedOutputs, nuConstant, weight, weightBar, cycles)
-    inputCounter = 1;
+inputIndex = 1;
+for index = 1:2
+    % inputs multiplied weight bars
+    activationVector1 =  wBar * y(:, inputIndex);
 
-    for index = 1:cycles
-        disp(size(weight));
-        disp(size(augmentedInputs));
-        activationVector =  weight * augmentedInputs; %augmentedInputs(:, inputCounter) * weight;
-        delta = calculateDelta(expectedOutputs(:, inputCounter), bipolarLogistic(activationVector), bipolarLogisticDifferential(activationVector));
-        disp(["delta", delta]);
-        
-        deltaBar = calculateDeltaBar(bipolarLogisticDifferential(activationVector), delta, weight);
-        disp(["deltaBar", deltaBar]);
-        
-        weight = weight + nuConstant * delta * augmentedInputs';
-        weightBar = weightBar + nuConstant * deltaBar * augmentedInputs';
-        inputCounter = inputCounter + 1;
-        
-        if (inputCounter > 4)
-            inputCounter = 1;
-        end
+    % activation outputs from the activation functions per element
+    layer1 = arrayfun(@bipolarLogisticFunction, activationVector1);
+
+    % then run layer 1 as inputs to the next layer
+    % add bias to layer 1
+    layer1(3, :) = bias;
+
+    activationVector2 = w * layer1;
+
+    %layer2 = arrayfun(@bipolarLogisticFunction, activationVector2);
+    layer2 = bipolarLogisticFunction(activationVector2);
+
+    % AKA delta
+    layer2Delta = (d(:, inputIndex) - layer2) * bipolarDifferentialFunction(activationVector2);
+
+    w = w + n * layer2Delta * activationVector2';% d(:, inputIndex);
+
+    % AKA deltaBar
+    layer1Delta = arrayfun(@bipolarDifferentialFunction, activationVector1) *...
+        sum(layer2Delta * wBar);
+    
+    %disp(layer1Delta);
+    disp(["sum", sum(layer2Delta * wBar)]);
+
+    activationVector1(3, :) = bias; % include a bias?
+
+    wBar = wBar + n * layer1Delta * activationVector1; %y(:, inputIndex)';
+    
+    disp(w);
+    disp(wBar);
+
+    inputIndex = inputIndex + 1;
+    if inputIndex > 4
+        inputIndex = 1;
     end
 end
 
-function output = calculateDelta(expectedOutput, activationValue, activationDifferential)
-    output = (expectedOutput - activationValue) * activationDifferential;
+function output = bipolarLogisticFunction(v)
+    output = 2 / (1 + exp(-v)) - 1;
 end
 
-function output = calculateDeltaBar(activationDifferential, delta, weights)
-    
-    output = activationDifferential * delta * weights;
-end
-
-function output = bipolarLogistic(v)
-    v = sum(v);
-    output = (2/(1 + exp(-v)) - 1);
-end
-
-function output = bipolarLogisticDifferential(v)
-    output = 0.5 * (1 - ((2/(1 + exp(-v)) - 1)^2));
+function output = bipolarDifferentialFunction(v)
+    output = 0.5 * (1 - (bipolarLogisticFunction(v))^2);
 end
