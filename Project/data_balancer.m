aeaf088185c91da93fed0da730577e7e1d95aa00
Joseph Tran <fred.smoth@gmail.com>
2017-06-08 14:29:01 +1000

    Able to split out some of the validation and testing data into training, can get roughly 50% in 10 cycles

---
+++ Project/data_balancer.m
@@ -0,0 +1,24 @@
+% goal is to divide the data up unevenly to provide more training data
+
+clc;
+clear;
+
+load('split_data.mat', 'overallTrainingData', 'overallValidationData', 'overallTestingData', 'expectedOutputs', 'gestureCount', 'totalSize');
+
+ratio = 0.5;
+
+[trainRows, trainCols] = size(overallTrainingData);
+[validRows, validCols] = size(overallValidationData);
+[testRows, testCols] = size(overallTestingData);
+
+%newOverallTrainingData = zeros(trainRows, trainCols + ratio * validCols, 
+
+newOverallTrainingData = [overallTrainingData, overallValidationData(:, validCols * ratio + 1:validCols), overallTestingData(:, testCols * ratio + 1:testCols)];
+newOverallValidationData = overallValidationData(:, 1:validCols * ratio);
+newOverallTestingData = overallTestingData(:, 1:testCols * ratio);
+
+overallTrainingData = newOverallTrainingData;
+overallValidationData = newOverallValidationData;
+overallTestingData = newOverallTestingData;
+
+save('split_data.mat', 'overallTrainingData', 'overallValidationData', 'overallTestingData', 'expectedOutputs', 'gestureCount', 'totalSize');
